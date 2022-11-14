ebd23e00cd44384a03664cf3fba6014389ca659e
Eti Koritz <ekoritz@g.jct.ac.il>
2022-11-14 15:18:36 +0200

    Initial code + Welcome functions

---
+++ Stage0/Program3904.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(String[]args)
+        {
+            Welcome3904();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome0000();
+        private static void Welcome3904()
+        {
+            Console.WriteLine("Enter your name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", userName);
+        }
+
+    }
+}
