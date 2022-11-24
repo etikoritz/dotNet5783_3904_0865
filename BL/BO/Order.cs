ca00dadc07d3e649dbcdc79337cd4c2cfcd1bdd0
Shirel <shkadosh@g.jct.ac.il>
2022-11-24 22:55:48 +0200

    OrderStatus fixed

--- BL/BO/Order.cs
+++ BL/BO/Order.cs
@@ -13,7 +13,7 @@ namespace BO
         public string CustomerEmail { get; set; }
         public string CustomerAddress { get; set; }
         public DateTime OrderDate { get; set; }
-        public OrderStatus Status{ get; set; }
+        public Enum.OrderStatus Status{ get; set; }
         public DateTime PaymentDate { get; set; }
         public DateTime ShipDate { get; set; }
         public DateTime DeliveryDate { get; set; }
--- BL/BO/OrderTracking.cs
+++ BL/BO/OrderTracking.cs
@@ -9,7 +9,7 @@ namespace BO
     public class OrderTracking
     {
         public int ID { get; set; }
-        public OrderStatus Status { get; set; }
+        public Enum.OrderStatus Status { get; set; }
 
         public override string ToString() => $@"
         Order ID: {ID} 
