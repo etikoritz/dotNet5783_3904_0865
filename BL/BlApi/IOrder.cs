1f8805f882540994a7e5a0fd2aa790bf66db98b2
Eti Koritz <ekoritz@g.jct.ac.il>
2023-02-15 00:05:48 +0200

    q

--- BL/BlApi/IOrder.cs
+++ BL/BlApi/IOrder.cs
@@ -63,7 +63,7 @@ namespace BlApi
         /// for the simulator - find the oldest order exist
         /// </summary>
         /// <returns>id of the oldest order</returns>
-        public int? GetOldestOrderID();
+        public BO.Order? GetOldestOrderID();
 
         //public int ChooseNextOrder();
     }
